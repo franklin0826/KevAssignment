b500d5a623b9e03ed6b7c0973d025f6a3de52b09
Franklin Mott <fmottprof@gmail.com>
2020-08-03 18:35:24 -0500

    quick refactor

--- SamsTakeHome/SamsTakeHome/Service/ArticleService.swift
+++ SamsTakeHome/SamsTakeHome/Service/ArticleService.swift
@@ -13,7 +13,7 @@ class ArticleService {
     let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-02&sortBy=publishedAt&apiKey=144ed5cdfe994830897159c56f275d82")
     func getArticles() {
         URLSession.shared.dataTask(with: url!) { (dat, resp, err) in
-            guard let error = err else {
+            guard err != nil else {
                 print("no error retrieved")
                 return
             }
@@ -25,7 +25,6 @@ class ArticleService {
                 return
             }
             print(articles)
-            
             guard let response = resp else {
                 print("response not found")
                 return
--- SamsTakeHome/SamsTakeHome/View Controllers/MainViewController.swift
+++ SamsTakeHome/SamsTakeHome/View Controllers/MainViewController.swift
@@ -12,14 +12,13 @@ class MainViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         searchBar.delegate = self
         searchBar.placeholder = "Search Articles"
-        
-        // Do any additional setup after loading the view.
     }
     
 
@@ -33,13 +32,16 @@ extension MainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell") as! ArticleCell
         // add cell logic in ArticleCell file
         return cell
-        
     }
+
     
     
 }
